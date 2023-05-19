0660b65ecf97db62fde533bcbc8e8bc5ae8c919b
aibat-t <tozhybayev.aibat@gmail.com>
2023-05-19 22:11:24 +0600

    preloader

--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -44,8 +44,9 @@ const moviesStore = {
       },
       root: true,
     },
-    async fetchMovies({ getters, commit }) {
+    async fetchMovies({ getters, commit, dispatch }) {
       try {
+        dispatch("toggleLoader", true, { root: true });
         const { currentPage, moviesPerPage, slicedIDs } = getters;
         const from = currentPage * moviesPerPage - moviesPerPage;
         const to = currentPage * moviesPerPage;
@@ -65,6 +66,8 @@ const moviesStore = {
         commit(MOVIES, movies);
       } catch (err) {
         console.log(err);
+      } finally {
+        dispatch("toggleLoader", false, { root: true });
       }
     },
     changeCurrentPage({ commit, dispatch }, page) {
